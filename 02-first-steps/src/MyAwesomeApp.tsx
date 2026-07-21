55b1728603db510ec02ada746c91559261fa595f
Gisella Franco <kikyomuok@gmail.com>
2026-07-21 22:51:14 +0000

    Impresión de variables

---
+++ 02-first-steps/src/MyAwesomeApp.tsx
@@ -0,0 +1,36 @@
+const firstName = 'Gisella';
+const lastName = 'Franco';
+
+const favoriteGames = ['Elden Ring', 'Smash', 'Metal Gear'];
+const isActive = false;
+
+const address = {
+  zipCode: 'ABC-123',
+  country: 'Canadá',
+};
+
+export const MyAwesomeApp = () => {
+  return (
+    <>
+      <h1>{firstName}</h1> 
+      <h3>{lastName}</h3>
+
+      <p>{favoriteGames.join(', ')}</p>
+      <p>{2 + 2}</p>
+
+      <h1>{isActive ? 'Activo' : 'No activo'}</h1>
+
+      <p>{JSON.stringify(address)}</p>
+
+    </>
+  );
+};
+
+// export function MyAwesomeApp() {
+//   return (
+//     <>
+//       <h1>Gisella</h1> 
+//       <h3>Franco</h3>
+//     </>
+//   );
+// }
