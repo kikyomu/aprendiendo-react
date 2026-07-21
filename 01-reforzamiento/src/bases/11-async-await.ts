0ea4264cff377bf6988ecd7dbf87922d3a33721c
Gisella Franco <kikyomuok@gmail.com>
2026-07-21 19:45:29 +0000

    Async - Await

---
+++ 01-reforzamiento/src/bases/11-async-await.ts
@@ -0,0 +1,22 @@
+import type { GiphyRandomResponse, Gif } from './data/giphy.response';
+
+const API_KEY = 'xhhvsabMk0yEOnx6Qr06TT1b3xEDx1Gw';
+
+const createImageInsideDOM = (url: string) => {
+    const imgElement = document.createElement('img');
+    imgElement.src = url;
+
+    document.querySelector('section')?.append(imgElement);
+};
+
+const getRandomGifUrl = async (): Promise<string> => {
+    const response = await fetch(
+        `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`
+    );
+
+    const { data }: GiphyRandomResponse = await response.json();
+
+    return data.images.original.url;
+};
+
+getRandomGifUrl().then(createImageInsideDOM);
--- 01-reforzamiento/src/main.ts
+++ 01-reforzamiento/src/main.ts
@@ -12,8 +12,8 @@ import './style.css'
 // import { getHeroesByOwner } from './bases/08-imp-exp';
 // import { Owner } from './bases/data/heroes.data';
 // import './bases/09-promises'
-import './bases/10-fetch-api'
-
+// import './bases/10-fetch-api'
+import './bases/11-async-await'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 <section id="center">
