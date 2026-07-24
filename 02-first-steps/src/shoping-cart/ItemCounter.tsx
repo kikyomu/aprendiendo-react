a8812cbdcda2ec913c8ae5be9e6760383762e5d4
Gisella Franco <kikyomuok@gmail.com>
2026-07-23 22:58:49 -0300

    Eventos de los elementos

--- 02-first-steps/src/shoping-cart/ItemCounter.tsx
+++ 02-first-steps/src/shoping-cart/ItemCounter.tsx
@@ -1,27 +1,33 @@
 interface Props {
-        name: string;
-        quantity?: number;
-    }; 
+    name: string;
+    quantity?: number;
+};
 
-const ItemCounter = ({name, quantity}: Props) => {
-  return (
-    <section style={{
-        display: 'flex',
-        alignItems: 'center',
-        gap: 10,
-        marginTop: 10,
-    }}
-    >
-        <span style={{
-            width: 150,
+const ItemCounter = ({ name, quantity }: Props) => {
+
+    const handleClick = () => {
+        console.log(`click en ${name}`);
+    }
+    return (
+        <section style={{
+            display: 'flex',
+            alignItems: 'center',
+            gap: 10,
+            marginTop: 10,
         }}
-        >{name}
-        </span>
-        <button>+1</button>
-        <span>{quantity}</span>
-        <button>-1</button>
-    </section>
-  )
+        >
+            <span style={{
+                width: 150,
+            }}
+            >{name}
+            </span>
+            <button
+                onClick={handleClick}
+            >+1</button>
+            <span>{quantity}</span>
+            <button>-1</button>
+        </section>
+    )
 }
 
 export default ItemCounter
