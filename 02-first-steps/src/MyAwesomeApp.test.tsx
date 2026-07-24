f746a7aac6a64de0fee62c782c5ed85a47c39a17
Gisella Franco <kikyomuok@gmail.com>
2026-07-24 16:27:39 -0300

    Buscar elementos en el componente renderizado

--- 02-first-steps/src/MyAwesomeApp.test.tsx
+++ 02-first-steps/src/MyAwesomeApp.test.tsx
@@ -1,13 +1,30 @@
-import { describe, test } from 'vitest';
+import { describe, expect, test } from 'vitest';
 import { render, screen } from '@testing-library/react';
 
 import { MyAwesomeApp } from './MyAwesomeApp';
 
 describe('MyAwesomeApp', () => {
   test('should render firstName and lastName', () => {
+    const { container } = render(<MyAwesomeApp />);
+    // screen.debug();
+    // console.log(container.innerHTML);
+
+    const h1 = container.querySelector('h1');
+    const h3 = container.querySelector('h3');
+
+    expect(h1?.innerHTML).toContain('Gisella');
+    expect(h3?.innerHTML).toContain('Franco');
+  });
+
+  test('should render firstName and lastName - screen', () => {
     render(<MyAwesomeApp />);
     screen.debug();
     // console.log(container.innerHTML);
 
+    // const h1 = screen.getByRole('heading', {
+    //   level: 1,
+    // });
+    const h1 = screen.getByTestId('first-name-title');
+    expect(h1.innerHTML).toContain('Gisella');
   });
 });
