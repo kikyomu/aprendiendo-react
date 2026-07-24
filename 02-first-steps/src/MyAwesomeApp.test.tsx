9fbd95cbc101dbe1f2ff028ed01c9dc6d566e4de
Gisella Franco <kikyomuok@gmail.com>
2026-07-24 16:41:57 -0300

    Evaluar 'snapshots'

--- 02-first-steps/src/MyAwesomeApp.test.tsx
+++ 02-first-steps/src/MyAwesomeApp.test.tsx
@@ -27,4 +27,14 @@ describe('MyAwesomeApp', () => {
     const h1 = screen.getByTestId('first-name-title');
     expect(h1.innerHTML).toContain('Gisella');
   });
+
+  test('should match snapshot', () => {
+    const { container } = render(<MyAwesomeApp />);
+    expect(container).toMatchSnapshot();
+  });
+
+  test('should match snapshot', () => {
+    render(<MyAwesomeApp />);
+    expect(screen.getByTestId('div-app')).toMatchSnapshot();
+  });
 });
