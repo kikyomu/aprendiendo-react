6f75205f21ee13df1295d05f50fcd2904390c03f
Gisella Franco <kikyomuok@gmail.com>
2026-07-24 00:52:00 -0300

    Archivos CSS

--- 02-first-steps/src/shoping-cart/ItemCounter.tsx
+++ 02-first-steps/src/shoping-cart/ItemCounter.tsx
@@ -1,4 +1,12 @@
+// 1- Importaciones de React
 import { useState } from "react";
+// 2- Importaciones de terceros
+
+// 3- Nuestras importaciones
+
+// 4 Importaciones de CSS
+import './ItemCounter.css';
+import styles from'./ItemCounter.module.css';
 
 interface Props {
     name: string;
@@ -8,40 +16,43 @@ interface Props {
 const ItemCounter = ({ name, quantity = 1 }: Props) => {
 
     //Los Hooks siempre van acá arriba
-    const [ count, setCount ] = useState(quantity);
+    const [count, setCount] = useState(quantity);
 
 
     const handleAdd = () => {
-        setCount(count+1);
-    }; 
+        setCount(count + 1);
+    };
 
     const handSubtract = () => {
         if (count === 0) return;
 
-        setCount(count-1);
+        setCount(count - 1);
     };
 
     return (
-        <section style={{
-            display: 'flex',
-            alignItems: 'center',
-            gap: 10,
-            marginTop: 10,
-        }}
+        <section 
+        className="item-row"
+        // style={{
+        // display: 'flex',
+        // alignItems: 'center',
+        // gap: 10,
+        // marginTop: 10,
+        // }}
         >
-            <span style={{
-                width: 150,
-            }}
+            <span className="item-text"
+            style={{
+                color: count === 1  ? 'red' : 'black',
+                }}
             >{name}
             </span>
-            <button
+            <button className={ styles['redButton'] }
                 onClick={handSubtract}
             >-1</button>
             <span>{count}</span>
-            <button
+            <button className={ styles.greenButton }
             onClick={handleAdd}
             >+1</button>
-        </section>
+        </section >
     )
 }
 
