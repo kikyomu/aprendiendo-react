92490d686eba671d4bfc6ba25139f9fce912435f
Gisella Franco <kikyomuok@gmail.com>
2026-08-04 15:55:02 -0300

    Estructura inicial, estilos y fuente

---
+++ 03-gifs-app/src/shared/components/CustomHeader.tsx
@@ -0,0 +1,13 @@
+
+export const CustomHeader = () => {
+    return (
+        <>
+            {/* Header */}
+            <div className="content-center">
+                <h1>Buscador de Gifs</h1>
+                <p>Descubre y comparte el gif perfecto</p>
+            </div>
+        </>
+    );
+};
+
