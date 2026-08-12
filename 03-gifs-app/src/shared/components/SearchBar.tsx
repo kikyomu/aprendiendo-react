bb63ff9c6330a869fe2833a6ba3a7881de3059e0
Gisella Franco <kikyomuok@gmail.com>
2026-08-12 00:11:10 -0300

    useEffect - Debounce

--- 03-gifs-app/src/shared/components/SearchBar.tsx
+++ 03-gifs-app/src/shared/components/SearchBar.tsx
@@ -1,4 +1,4 @@
-import { useState, type KeyboardEvent } from "react";
+import { useEffect, useState, type KeyboardEvent } from "react";
 
 interface Props {
     placeholder?: string;
@@ -9,9 +9,19 @@ interface Props {
 export const SearchBar = ({ placeholder = 'Buscar', onQuery }: Props ) => {
     const [query, setQuery] = useState('');
 
+    useEffect(() => {
+        const timeoutId = setTimeout(() => {
+            onQuery(query)
+        }, 700);
+
+        return() => {
+            clearTimeout(timeoutId);
+        };
+    }, [query, onQuery]);
+
     const handleSearch = () => {
         onQuery(query);
-        setQuery('');
+        //setQuery('');
     };
 
     const handleKeyDown = (event: KeyboardEvent<HTMLInputElement>) => {
