89edae6ac7ec4c21301f90a4458ee90bdf99da41
Gisella Franco <kikyomuok@gmail.com>
2026-08-19 21:21:45 -0300

    Obtener gifs mediante petición http

--- 03-gifs-app/src/GifsApp.tsx
+++ 03-gifs-app/src/GifsApp.tsx
@@ -4,15 +4,16 @@ import { GifList } from "./gifs/components/GifList"
 import { PreviousSearches } from "./gifs/components/PreviousSearches"
 import { mockGifs } from "./mock-data/gifs.mock"
 import { useState } from "react"
+import { getGifsByQuery } from "./gifs/actions/get-gifs-by-query.action"
 
 export const GifsApp = () => {
-
     const [previousTerms, setPreviousTerms] = useState(['dragon ball z']);
+    
     const handleTermClicked = (term: string) => {
         console.log({ term });
     };
 
-    const handleSearch = (query: string = '') => {
+    const handleSearch = async (query: string = '') => {
 // Tarea:Implementar la función handleSearch que debe:
 // Convertir el query a minúsculas y eliminar espacios en blanco
     query = query.trim().toLowerCase();
@@ -23,6 +24,10 @@ export const GifsApp = () => {
     if (previousTerms.includes(query)) return;
 // Actualizar previousTerms agregando el nuevo término al inicio y limitando a 8 elementos máximo, es decir no puede ser un arreglo de más de 8.
     setPreviousTerms([query, ...previousTerms].splice(0, 8));
+    
+    const gifs = await getGifsByQuery(query);
+
+    console.log({gifs});
     };
 
     return (
