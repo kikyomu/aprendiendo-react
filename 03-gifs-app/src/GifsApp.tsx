b1b31af0e06ccd8db9cf179ad85ae63422a14c72
Gisella Franco <kikyomuok@gmail.com>
2026-08-19 22:03:09 -0300

    Solución a la tarea - Mostrar Gifs

--- 03-gifs-app/src/GifsApp.tsx
+++ 03-gifs-app/src/GifsApp.tsx
@@ -1,13 +1,17 @@
-import { CustomHeader } from "./shared/components/CustomHeader"
-import { SearchBar } from "./shared/components/SearchBar"
-import { GifList } from "./gifs/components/GifList"
-import { PreviousSearches } from "./gifs/components/PreviousSearches"
-import { mockGifs } from "./mock-data/gifs.mock"
-import { useState } from "react"
-import { getGifsByQuery } from "./gifs/actions/get-gifs-by-query.action"
+import { useState } from "react";
+
+import { GifList } from "./gifs/components/GifList";
+import { PreviousSearches } from "./gifs/components/PreviousSearches";
+
+import { CustomHeader } from "./shared/components/CustomHeader";
+import { SearchBar } from "./shared/components/SearchBar";
+
+import { getGifsByQuery } from "./gifs/actions/get-gifs-by-query.action";
+import type { Gif } from "./gifs/interfaces/gif.interface";
 
 export const GifsApp = () => {
-    const [previousTerms, setPreviousTerms] = useState(['dragon ball z']);
+    const [gifs, setGifs] = useState<Gif[]>([]);
+    const [previousTerms, setPreviousTerms] = useState<string[]>([]);
     
     const handleTermClicked = (term: string) => {
         console.log({ term });
@@ -27,7 +31,7 @@ export const GifsApp = () => {
     
     const gifs = await getGifsByQuery(query);
 
-    console.log({gifs});
+    setGifs(gifs);
     };
 
     return (
@@ -51,7 +55,7 @@ export const GifsApp = () => {
             />
 
             {/* Gifs */}
-            <GifList gifs={mockGifs} />
+            <GifList gifs={gifs} />
 
         </>
     )
--- 03-gifs-app/src/gifs/components/GifList.tsx
+++ 03-gifs-app/src/gifs/components/GifList.tsx
@@ -1,4 +1,5 @@
-import { mockGifs, type Gif } from "../../mock-data/gifs.mock"
+import type { FC } from "react";
+import type { Gif } from "../interfaces/gif.interface";
 
 interface Props {
     gifs: Gif[];
@@ -8,7 +9,7 @@ export const GifList: FC<Props> = ({ gifs }) => {
     return (
         <div className="gifs-container">
             {
-                mockGifs.map((gif) => (
+                gifs.map((gif) => (
                     <div key={gif.id} className="gif-card">
                         <img src={gif.url} alt={gif.title} />
                         <h3>{gif.title}</h3>
